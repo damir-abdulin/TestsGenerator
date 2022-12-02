fc0dcdc95321669873ff2e70a4a3763d2e1431b6
Damir Abdulin <damir.abdulin@tutanota.com>
2022-12-02 17:31:47 +0300

    ADD: Base for system

--- TestsGeneratorCore/TestsGenerator.cs
+++ TestsGeneratorCore/TestsGenerator.cs
@@ -1,8 +1,50 @@
 ﻿namespace TestsGeneratorCore;
 
-public class TestsGenerator : ITestsGenerator
+public static class TestsGenerator
 {
-    public void GenerateTests(string inputDirectory, string outputDirectory, params string[] fileNames)
+    private const int MaxInputFiles = 5;
+    private const int MaxOutputFiles = 5;
+    
+    public static void GenerateTests(string inputDirectory, string outputDirectory, params string[] fileNames)
+    {
+        var inputFiles = GetFullFilesPath(inputDirectory, fileNames);
+        LoadFilesAsync(inputFiles);
+        
+        GenerateTestsAsync();
+        
+        var outputFiles = GetFullFilesPath(outputDirectory, fileNames);
+        WriteFilesAsync(outputFiles);
+    }
+
+    private static string[] GetFullFilesPath(string directory, params string[] files)
+    {
+        return files.Select(filename => $"{directory}\\{filename}").ToArray();
+    }
+
+    private static void LoadFile(string filePath)
+    {
+        throw new NotImplementedException();
+    }
+    private static void GenerateTest(string file)
+    {
+        throw new NotImplementedException();
+    }
+    private static void WriteFile(string file)
+    {
+        throw new NotImplementedException();
+    }
+
+    private static void LoadFilesAsync(params string[] filesPaths)
+    {
+        throw new NotImplementedException();
+    }
+
+    private static void GenerateTestsAsync(params string[] files)
+    {
+        throw new NotImplementedException();
+    }
+    
+    private static void WriteFilesAsync(params string[] files)
     {
         throw new NotImplementedException();
     }
