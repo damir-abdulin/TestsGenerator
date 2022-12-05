9195f7af48512eeee0417e248ea2721feb20395d
Damir Abdulin <damir.abdulin@tutanota.com>
2022-12-05 10:49:44 +0300

    ADD: Create methods for loading tests

--- TestsGeneratorsCui/Program.cs
+++ TestsGeneratorsCui/Program.cs
@@ -1,3 +1,78 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Runtime.InteropServices.ComTypes;
 
-Console.WriteLine("Hello, World!");
+namespace TestsGeneratorsCui;
+
+public static class Program
+{
+    public static async Task Main(string[] args)
+    {
+        if (!CheckArgsCount(args.Length)) return;
+
+        var inputFiles = GetInputFiles(args[0]);
+        var outputDirectory = args[1];
+
+        var degreeOfParallelismRead = GetDegreesOfParallelism(args[2]);
+        if (degreeOfParallelismRead == -1)
+        {
+            Console.WriteLine($"Invalid degree of parallelism READ. Expected integer, got {args[2]}");
+            return;
+        }
+        
+        var degreeOfParallelismGenerate = GetDegreesOfParallelism(args[2]);
+        if (degreeOfParallelismGenerate == -1)
+        {
+            Console.WriteLine($"Invalid degree of parallelism GENERATE. Expected integer, got {args[3]}");
+            return;
+        }
+        
+        var degreeOfParallelismWrite = GetDegreesOfParallelism(args[2]);
+        if (degreeOfParallelismWrite == -1)
+        {
+            Console.WriteLine($"Invalid degree of parallelism WRITE. Expected integer, got {args[4]}");
+            return;
+        }
+
+        var testsGeneratorService = new TestsGeneratorService(degreeOfParallelismRead, degreeOfParallelismGenerate,
+            degreeOfParallelismWrite, outputDirectory);
+        await testsGeneratorService.Generate(inputFiles);
+    }
+
+    private static bool CheckArgsCount(int argsLength)
+    {
+        if (argsLength == 5) return true;
+        
+        
+        Console.WriteLine("[ERROR] Not enough arguments." +
+                          "Usage: <input files separated with '|'> <output directory> " +
+                          "<degree of parallelism READ> <degree of parallelism GENERATE>" +
+                          "<degree of parallelism WRITE>");
+        return false;
+
+    }
+    private static List<string> GetInputFiles(string inputFiles)
+    {
+        var userInputFiles = inputFiles.Split('|');
+        var correctInputFiles = new List<string>();
+        foreach (var inputFile in userInputFiles)
+        {
+            if (File.Exists(inputFile))
+            {
+                correctInputFiles.Add(inputFile);
+            }
+            else
+            {
+                Console.WriteLine($"File '{inputFile}' doesn't exist or you don't have permissions to open it. " +
+                                  "It would be removed from generating");
+            }
+        }
+
+        return correctInputFiles;
+    }
+    private static int GetDegreesOfParallelism(string arg)
+    {
+        if (int.TryParse(arg, out var degreeOfParallelism))
+            return degreeOfParallelism;
+        
+        return -1;
+    }
+}
---
+++ TestsGeneratorsCui/TestsGeneratorService.cs
@@ -0,0 +1,75 @@
+﻿using System.Net.Sockets;
+using System.Security;
+using System.Threading.Tasks.Dataflow;
+using TestsGeneratorCore;
+
+namespace TestsGeneratorsCui;
+
+/// <summary>
+/// Reads, generates and writes generated tests.
+/// </summary>
+public class TestsGeneratorService
+{
+    private readonly TestsGenerator _testsGenerator = new();
+
+    private readonly TransformBlock<string, string> _readerBlock;
+    private readonly TransformManyBlock<string, string> _generatorBlock;
+    private readonly ActionBlock<string> _writerBlock;
+    
+    /// <summary>
+    /// Constructor for service
+    /// </summary>
+    /// <param name="degreeOfParallelismRead">degree of parallelism for input files reading</param>
+    /// <param name="degreeOfParallelismGenerate">degree of parallelism for tests generation</param>
+    /// <param name="degreeOfParallelismWrite">>degree of parallelism for writes tests to output files</param>
+    /// <param name="outputDirectory">directory with generated tests.</param>
+    public TestsGeneratorService(int degreeOfParallelismRead, int degreeOfParallelismGenerate,
+        int degreeOfParallelismWrite, string outputDirectory)
+    {
+        _readerBlock = InitReadBlock(degreeOfParallelismRead);
+        _generatorBlock = InitGenerateBlock(degreeOfParallelismGenerate);
+        _writerBlock = InitWriteBlock(degreeOfParallelismWrite, outputDirectory);
+
+        _readerBlock.LinkTo(_generatorBlock, new DataflowLinkOptions { PropagateCompletion = true });
+        _generatorBlock.LinkTo(_writerBlock, new DataflowLinkOptions { PropagateCompletion = true });
+    }
+
+    /// <summary>
+    /// Launch tests generation.
+    /// </summary>
+    /// <param name="fileNames">Input files with classes</param>
+    public async Task Generate(List<string> fileNames)
+    {
+        foreach (var fileName in fileNames)
+        {
+            _readerBlock.Post(fileName);
+        }
+        
+        _readerBlock.Complete();
+        await _writerBlock.Completion;
+    }
+
+    private TransformBlock<string, string> InitReadBlock(int degreeOfParallelism)
+    {
+        return new TransformBlock<string, string>(async filename =>
+        {
+            using var reader = File.OpenText(filename);
+            return await reader.ReadToEndAsync();
+        }, new ExecutionDataflowBlockOptions { MaxDegreeOfParallelism = degreeOfParallelism });
+    }
+
+    private TransformManyBlock<string, string> InitGenerateBlock(int degreeOfParallelism)
+    {
+        return new TransformManyBlock<string, string>(source => _testsGenerator.Generate(),
+            new ExecutionDataflowBlockOptions { MaxDegreeOfParallelism = degreeOfParallelism });
+    }
+
+    private ActionBlock<string> InitWriteBlock(int degreeOfParallelism, string outputDirectory)
+    {
+        return new ActionBlock<string>(async classInfo =>
+        {
+            await using var writer = new StreamWriter($"{outputDirectory}\\test.txt");
+            await writer.WriteAsync(classInfo);
+        }, new ExecutionDataflowBlockOptions { MaxDegreeOfParallelism = degreeOfParallelism });
+    }
+}
